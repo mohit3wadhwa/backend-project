c5f272867e5a732d797ed6d5eb1def7a73119a6b
Mohit Wadhwa <mohitwadhwa@Mohits-MacBook-Air.local>
2021-03-07 15:16:46 +0530

    Refactoring Code

--- apis/main.py
+++ apis/main.py
@@ -1,9 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 
 app = FastAPI()
 
+fakeDB = {}
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
 @app.get("/")
-def read_home():
-    return{"hello": "There!"}
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    for key in fakeDB.keys():
+        if item_id == key:
+            #return{"item_id": item_id, "value": value}
+            return {"item": fakeDB[key]}
+    #return{"no items found"}
+    raise HTTPException(status_code=404, detail="Item not found")
+
+@app.get("/items")
+def read_all_items():
+    if not fakeDB:
+        raise HTTPException(status_code=404, detail="No Items found")
+    else:
+        return fakeDB
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    fakeDB[item_id] = item
+    #print(fakeDB)
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    for key in fakeDB.keys():
+        if item_id == key:
+            del fakeDB[item_id]
+            return{"Success! Item ",item_id," deleted successfully"}
+    raise HTTPException(status_code=404, detail="Item not found for deletion")
